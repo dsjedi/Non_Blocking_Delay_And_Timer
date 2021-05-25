74b4dffa848a74a1ecc515dee2b4d715a8b4a7a2
Digvijay Singh Jadaun <69453051+dsjedi@users.noreply.github.com>
2021-05-25 17:10:01 +0530

    Add files via upload

---
+++ nbdelay.cpp
@@ -0,0 +1,61 @@
+#include "nbdelay.h"
+
+Nonblockdelay::Nonblockdelay()
+{
+	delayFlag.time = 1000;
+  delayFlag.premillis=0; 
+  _indelay = false;
+}
+
+boolean Nonblockdelay::done()
+{
+  if(!this-> delayFlag.started) 
+    return false;
+
+  if( (millis() - this-> delayFlag.premillis) > this-> delayFlag.time){
+    this->stop();
+    return true;
+  }
+  return false;
+}
+
+void Nonblockdelay:: set(unsigned long t)
+{
+    this->delayFlag.time = t;
+}
+
+void Nonblockdelay::_nbTimer()
+{
+  
+  this->reset();
+  this->delayFlag.started = true;
+  this-> _indelay = true;
+}
+
+void Nonblockdelay::reset()
+{
+  this->stop();
+  this->delayFlag.premillis = millis();
+}
+
+void Nonblockdelay::stop()
+{
+  this->delayFlag.started = false;
+  this-> _indelay = false;
+
+}
+
+boolean Nonblockdelay::indelay(){
+  return (this->delayFlag.started && !this->done()) ? true : false;
+}
+
+void Nonblockdelay::nbdelay(unsigned long time, method function)
+{
+  this->delayFlag.time = time;
+  delayFlag.now = millis();
+  if( (this->delayFlag.now - this-> delayFlag.premillis) > this-> delayFlag.time)
+  {
+    delayFlag.premillis= delayFlag.now;
+    function();
+  }
+}
---
+++ nbdelay.h
@@ -0,0 +1,28 @@
+#include <Arduino.h>  
+
+typedef void (*method)(void);
+
+class Nonblockdelay
+{
+    public:
+        Nonblockdelay();
+        boolean done();
+        boolean indelay();	
+	    void stop();	
+	    void reset();
+        void set(unsigned long t);           
+	    void _nbTimer();
+        void nbdelay(unsigned long , method);
+    private:
+        struct Nbd
+        {
+		    unsigned long time;
+		    unsigned long premillis;
+            unsigned long now; 
+		    boolean done;
+		    boolean started;
+	    };
+        Nbd delayFlag;
+	    boolean _indelay;
+        method  function;
+};
